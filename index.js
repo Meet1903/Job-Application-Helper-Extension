d904f1dba1c7226fe66bbf5547d9cb61953f6673
Meet1903 <meet.diwan.md@gmail.com>
2024-06-21 00:05:36 -0400

    More refined JS code

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const inputNoteEl = document.getElementById("input-note")
 const saveBtnEl = document.getElementById("save-btn")
 const noteDisplayEl = document.getElementById("note-display")
+let notes = []
+let noteObject = {
+    url: "",
+    note: ""
+}
 
 saveBtnEl.addEventListener("click", function() {
-    let listItem = ""
-    let note = inputNoteEl.value
-    if (note) {
-        listItem = `
-            <li> ${note} </li>
-        `
+    let url = ""
+    clearNoteObject()
+    if (chrome && chrome.tabs){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            noteObject.url = tabs[0].url
+        })
     }
-    noteDisplayEl.innerHTML += listItem
-    inputNoteEl.value = ""
+    noteObject.note = inputNoteEl.value
+    addNote(noteObject)
 })
+
+function addNote(noteObject) {
+    notes.push(noteObject)
+    clearNoteObject()
+    renderNotes()
+    inputNoteEl.value = ""
+}
+
+function clearNoteObject() {
+    noteObject= {
+        url: "",
+        note: ""
+    }
+}
+
+function renderNotes() {
+    let listItem = ""
+    notes.map((noteObject) => {
+        if (noteObject.note) {
+            listItem += `
+                <li> ${noteObject.note} </li>
+            `
+        }
+        
+    })
+    noteDisplayEl.innerHTML = listItem
+}
